f8273261ed81f9185905e3896fddf9d53ab5e906
giaco <giaco14mw@gmail.com>
2025-08-18 01:09:37 +0200

    Parser json

--- log_monitor.py
+++ log_monitor.py
@@ -1,5 +1,4 @@
 import json
-from sys import stdin, stdout, stderr
 
 import paramiko
 
@@ -7,11 +6,15 @@ ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client .connect("192.168.1.250", 22, "root","opnsense") # L'indirizzo IP andrà cambiato poi
 
-stdin, stdout, stderr = ssh_client.exec_command("cat /var/log/suricata/eve.json")
-log_data = stdout.read().decode()
-
-print(log_data[:500])
+stdin, stdout, stderr = ssh_client.exec_command("tail -n 100 /var/log/suricata/eve.json")
+# log_data = stdout.read().decode()
+#
+# print(log_data)
 # decoded_log_data = json.loads(log_data)
+
+for line in stdout:
+    event = json.loads(line.strip())
+    print(event)
 
 ssh_client.close()
 
