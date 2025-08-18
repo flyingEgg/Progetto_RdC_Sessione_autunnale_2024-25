97f13a4f85c3bd70937bbb22aa3386447b260e4b
giaco <giaco14mw@gmail.com>
2025-08-18 15:42:02 +0200

    Reformat code: add functions for SSH connection & fetching logs content.

--- log_monitor.py
+++ log_monitor.py
@@ -2,20 +2,49 @@ import json
 
 import paramiko
 
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client .connect("192.168.1.250", 22, "root","opnsense") # L'indirizzo IP andrà cambiato poi
 
-stdin, stdout, stderr = ssh_client.exec_command("tail -n 100 /var/log/suricata/eve.json")
-# log_data = stdout.read().decode()
-#
-# print(log_data)
-# decoded_log_data = json.loads(log_data)
+class SuricataAnalyzer:
+    def __init__(self, ssh_host="192.168.1.250", ssh_port=22, ssh_user="root", ssh_pass="opnsense"):
+        self.ssh_host = ssh_host
+        self.ssh_port = ssh_port
+        self.ssh_user = ssh_user
+        self.ssh_pass = ssh_pass
 
-for line in stdout:
-    event = json.loads(line.strip())
-    print(event)
+    def connect_ssh(self):
+        try:
+            self.ssh_client = paramiko.SSHClient()
+            self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh_client.connect(self.ssh_host, self.ssh_port, self.ssh_user, self.ssh_pass)
+            print("Connesso ad SSH")
+            return True
+        except Exception as e:
+            print(f"Errore di connessione ad SSH: {e}")
+            return False
 
-ssh_client.close()
+    def fetch_logs(self):
+        try:
+            stdin, stdout, stderr = self.ssh_client.exec_command(f"cat /var/log/suricata/eve.json")
 
-# ???
+            events = []
+            for line in stdout:
+                try:
+                    event = json.loads(line.strip())
+                    events.append(event)
+                except json.decoder.JSONDecodeError:
+                    continue
+
+                print(f"Recuperati {len(events)} eventi da analizzare")
+                return events
+        except Exception as e:
+            print(f"Errore di lettura log: {e}")
+            return []
+
+    # stdin, stdout, stderr = ssh_client.exec_command("tail -n 100 /var/log/suricata/eve.json")
+    # log_data = stdout.read().decode()
+    #
+    # print(log_data)
+    # decoded_log_data = json.loads(log_data)
+
+    def close_conn(self):
+        self.ssh_client.close()
+
